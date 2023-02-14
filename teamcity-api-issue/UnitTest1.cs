b23fbc2153c58df9a5bc016f8286ca6729287a98
Alexander Salamatov <ch1seL@ya.ru>
2023-02-14 09:36:30 +0300

    add logging and api deplay parameter

--- teamcity-api-issue/UnitTest1.cs
+++ teamcity-api-issue/UnitTest1.cs
@@ -4,6 +4,7 @@ public class Tests
 {
     private HttpClient? _httpClient;
     private string? _buildId;
+    private string? _apiDelay;
 
     [OneTimeSetUp]
     public void SetUpFixture()
@@ -11,6 +12,7 @@ public class Tests
         var username = Environment.GetEnvironmentVariable("TeamcityUsername");
         var password = Environment.GetEnvironmentVariable("TeamcityPassword");
         _buildId = Environment.GetEnvironmentVariable("TeamcityBuildId");
+        _apiDelay = Environment.GetEnvironmentVariable("TeamCityApiDelay");
 
         _httpClient = new HttpClient
         {
@@ -27,24 +29,34 @@ public class Tests
     [SetUp]
     public void Setup()
     {
+        Console.WriteLine($"{DateTime.Now:O}: Test {TestContext.CurrentContext.Test.Name} Setup");
     }
 
-    [Test, Order(0)]
+    [Test]
+    [Order(0)]
     public void SuccessTest()
     {
         Assert.Pass();
     }
 
-    [Test, Order(1)]
+    [Test]
+    [Order(1)]
     public async Task TeamCitySuccessTestCountIsOne()
     {
-        await Task.Delay(TimeSpan.FromSeconds(20));
-        
+        await Task.Delay(TimeSpan.Parse(_apiDelay!));
+
         var result =
-            await _httpClient!.GetFromJsonAsync<JsonElement>($"testOccurrences?locator=build:{_buildId},status:SUCCESS");
+            await _httpClient!.GetFromJsonAsync<JsonElement>(
+                $"testOccurrences?locator=build:{_buildId},status:SUCCESS");
         var numberOfTests = result.GetProperty("testOccurrence").EnumerateArray().Count();
 
         Assert.That(numberOfTests, Is.EqualTo(1));
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Console.WriteLine($"{DateTime.Now:O}: Test {TestContext.CurrentContext.Test.Name} TearDown");
     }
 
     [OneTimeTearDown]
